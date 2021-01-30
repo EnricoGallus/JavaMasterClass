eb2f591b086607577a68476d80484ca3c2097a5a
Enrico Gallus <enrico.gallus@gmail.com>
2021-01-30 17:27:02 +0900

    primitive data types - char and boolean - finished challenge

---
+++ Section3_4CharAndBoolean/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyright = '\u00A9';
+        System.out.println(myCopyright);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
