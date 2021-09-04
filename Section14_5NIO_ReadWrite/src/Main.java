dfe9b783a22de7079b62c934a39f0b1993e88c5e
Enrico Gallus <enrico.gallus@gmail.com>
2021-09-05 06:37:44 +0900

    using pipes

--- Section14_5NIO_ReadWrite/src/Main.java
+++ Section14_5NIO_ReadWrite/src/Main.java
@@ -3,10 +3,69 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
+
+        try {
+           Pipe pipe = Pipe.open();
+
+           Runnable writer = new Runnable() {
+               @Override
+               public void run() {
+                   try {
+                        Pipe.SinkChannel sinkChannel = pipe.sink();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+                        for (int i=0; i<10; i++) {
+                            String currentTime = "The time is: " + System.currentTimeMillis();
+                            buffer.put(currentTime.getBytes(StandardCharsets.UTF_8));
+                            buffer.flip();
+
+                            while (buffer.hasRemaining()) {
+                                sinkChannel.write(buffer);
+                            }
+
+                            buffer.flip();
+
+                            Thread.sleep(100);
+                        }
+                   } catch (Exception e) {
+                       e.printStackTrace();
+                   }
+               }
+           };
+
+           Runnable reader = new Runnable() {
+               @Override
+               public void run() {
+                   try {
+                        Pipe.SourceChannel sourceChannel = pipe.source();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+                       for (int i=0; i<10; i++) {
+                           int bytesToRead = sourceChannel.read(buffer);
+                           byte[] timeString = new byte[bytesToRead];
+                           buffer.flip();
+                           buffer.get(timeString);
+                           System.out.println("Reader Thread: " + new String(timeString));
+                           buffer.flip();
+                           Thread.sleep(100);
+                       }
+
+                   } catch (Exception e) {
+                       e.printStackTrace();
+                   }
+               }
+           };
+
+           new Thread(writer).start();
+           new Thread(reader).start();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         try (FileOutputStream binFile = new FileOutputStream("Section14_5NIO_ReadWrite/data2.dat");
              FileChannel binChannel = binFile.getChannel()) {
 
@@ -50,6 +109,17 @@ public class Main {
             channel.read(readBuffer);
             readBuffer.flip();
             System.out.println("int1 = " + readBuffer.getInt());
+
+            RandomAccessFile copyFile = new RandomAccessFile("Section14_5NIO_ReadWrite/datacopy.dat", "rw");
+            FileChannel copyChannel = copyFile.getChannel();
+            channel.position(0);
+            //long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+            System.out.println("Num transferred = " + numTransferred);
+
+            channel.close();
+            ra.close();
+            copyChannel.close();
 
             /*ByteBuffer readBuffer = ByteBuffer.allocate(100);
             channel.read(readBuffer);
